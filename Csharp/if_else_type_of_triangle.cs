d5eadbb884fe552516a6a60ac739b243641a7243
Vrushali Mohite <vrushalimohite223@gmail.com>
2023-10-18 23:40:40 +0530

    csharp

---
+++ Csharp/if_else_type_of_triangle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace program
+{
+    class triangle
+    {
+        static void Main()
+        {
+            int a, b, c;
+            Console.WriteLine("Enter angles of triangles <a,<b,<c");
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            c= Convert.ToInt32(Console.ReadLine());
+
+            if (a==b && b==c)
+            {
+                Console.WriteLine("This is an E Triangle");
+            }
+            else if(a==b || b==c || c==a )
+            {
+                Console.WriteLine("This is an T Triangle");
+
+            }
+            else
+            {
+                Console.WriteLine("This is an Scalence Triangle");
+
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Csharp/if_else_vowel.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+namespace program
+{
+    class vowel
+    {
+        static void Main()
+        {
+            char ch;
+            Console.WriteLine("Enter vowel");
+            ch = Convert.ToChar(Console.ReadLine());
+
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' 
+                || ch == 'u' || ch=='A' || ch=='E' || ch=='I' || ch=='O' || ch=='U') 
+            {
+                Console.WriteLine("It is vowel");
+            }
+            else
+            {
+                Console.WriteLine("It is not vowel");
+
+            }
+            Console.ReadKey();
+        }
+    }
+
+}
