2e4d4e91db8746622b746fc6077184a656d720f2
Vrushali Mohite <vrushalimohite223@gmail.com>
2023-10-18 13:14:00 +0530

    local

---
+++ Csharp/age_older_tweentyfive.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace program
+{
+class age
+{
+    static void Main()
+    {
+        int num;
+        Console.WriteLine("Enter no");
+        num = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Your Look older than 25");
+        Console.ReadKey();
+    }
+
+}
+}
---
+++ Csharp/xyz_eqn.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace program
+{
+    class eqn
+    {
+        static void Main()
+        {
+
+            int x, y, z, result1,result2;
+            Console.WriteLine("Enter x,y,z values");
+            x = Convert.ToInt32(Console.ReadLine());
+            y = Convert.ToInt32(Console.ReadLine());
+            z = Convert.ToInt32(Console.ReadLine());
+            result1 = (x + y) * z;
+            Console.WriteLine("result1 is=" + result1);
+            result2 = (x * y )+(y * z);
+            Console.WriteLine("result2 is=" + result2);
+            Console.ReadKey();
+
+        }
+     
+    }
+}
