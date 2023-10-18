60883d22e4e468d704a8b852d0c9400d36c8dfd3
Vrushali Mohite <vrushalimohite223@gmail.com>
2023-10-18 14:35:29 +0530

    csharp

---
+++ Csharp/bodmas_eqn.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace program
+{
+    class bodmas
+    {
+        static void Main()
+        {
+            int result;
+            result = -1 + 4 * 6;
+            Console.WriteLine("result is =" + result);
+            result = (35 + 5) % 7;
+            Console.WriteLine("result is =" + result);
+            result = 14 + 4 * 6 / 11;
+            Console.WriteLine("result is =" + result);
+            result = 2 + 15 / 6 * 1 - 7 % 2;
+            Console.WriteLine("result is =" + result);
+            Console.ReadKey();
+
+        }
+    }
+}
