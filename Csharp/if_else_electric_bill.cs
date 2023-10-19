adcb86d7fc81bcd56da73bd886f0c4414efbb28f
Vrushali Mohite <vrushalimohite223@gmail.com>
2023-10-19 16:42:27 +0530

    csharp

---
+++ Csharp/if_else_electric_bill.cs
@@ -0,0 +1,66 @@
+﻿using System;
+namespace program
+{
+    class bill
+    {
+        static void Main()
+        {
+            string name;
+            int idno;
+            float unit;
+            double total=0, surchage = 0, netamt;
+            Console.WriteLine(" Customer Name");
+            name=(Console.ReadLine());
+
+            Console.WriteLine(" Customer Id no");
+            idno = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Unit Consumed");
+            unit = Convert.ToInt32(Console.ReadLine());
+             if(unit>0 && unit<=199)
+            {
+                total = unit * 1.20f;
+
+
+            }
+            else if( unit>=200 && unit<=400)
+            {
+                total = unit * 1.50f;
+
+            }
+            else if(unit>400 && unit<=600)
+            {
+                total = unit * 1.80f;
+
+            }
+            else if(unit>600)
+            {
+                total = unit * 2.00f;
+
+            }
+             if(total>=400)
+            
+            surchage = total * 15 / 100.0;
+            netamt = total + surchage;
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine("Customer Name :" + " " + name);
+            Console.WriteLine("Customer idno :" + " " + idno);
+            Console.WriteLine("unit Consumed :" + " " + unit);
+            Console.WriteLine("Amount Charge :" +" "+ total);
+            Console.WriteLine("Surchage Amount :" +" "+ surchage);
+
+            Console.WriteLine("Net Amount paid by the customed  :" + netamt);
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ Csharp/if_else_menu_driven_operator.cs
@@ -0,0 +1,77 @@
+﻿using System;
+namespace program
+{
+    class operation
+    {
+        static void Main()
+        {
+            int num1, num2;
+            float result;
+            int choice;
+            Console.WriteLine("Enter Choice 1 for Addition");
+            Console.WriteLine("Enter Choice 2 for Substraction");
+            Console.WriteLine("Enter Choice 3 for Multiplication");
+            Console.WriteLine("Enter Choice 4 for Division");
+            Console.WriteLine("Enter Choice 5 Exit");
+
+
+            choice = Convert.ToInt32(Console.ReadLine());
+
+            if (choice == 1)
+            {
+                Console.WriteLine("Enter number 1");
+                num1 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Enter number 2");
+
+                num2 = Convert.ToInt32(Console.ReadLine());
+
+                result = num1 + num2;
+                Console.WriteLine("Addition :" + " " + result);
+            }
+            else if (choice == 2)
+            {
+                Console.WriteLine("Enter number 1");
+                num1 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Enter number 2");
+                num2= Convert.ToInt32(Console.ReadLine());
+                result = num1 - num2;
+                Console.WriteLine(" Substraction :" + " " + result);
+            }
+            else if (choice == 3)
+            {
+                Console.WriteLine("Enter number 1");
+                num1 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Enter number 2");
+                num2= Convert.ToInt32(Console.ReadLine());
+                result = num1 * num2;
+                Console.WriteLine("Multiplication :" + " " + result);
+            }
+            else if (choice == 4)
+            {
+                Console.WriteLine("Enter number 1");
+                num1 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Enter number 2");
+                num2= Convert.ToInt32(Console.ReadLine());
+                result = num1 / num2;
+                Console.WriteLine("Division :" + " " + result);
+            }
+            else if(choice==5)
+            {
+                Console.WriteLine("Exit");
+
+            }
+            else
+            {
+                Console.WriteLine("Invalid choice number");
+
+            }
+        }
+    }
+}
+
+
+
