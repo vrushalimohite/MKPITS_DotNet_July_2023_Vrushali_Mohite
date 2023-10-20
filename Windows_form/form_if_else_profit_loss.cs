3dd486d367e88b02d150567ddcc0fe31d5795e8e
Vrushali Mohite <vrushalimohite223@gmail.com>
2023-10-21 00:00:19 +0530

    windows

---
+++ Windows_form/form_if_else_profit_loss.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace form_if_else_profit_loss
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int c, s;
+            int result;
+            c = Convert.ToInt32(textBox1.Text);
+            s = Convert.ToInt32(textBox2.Text);
+            if(s>c)
+            {
+                result = s - c;
+                label3.Text="Profit :"+result;
+            }
+            else 
+            {
+                result = c - s;
+                label3.Text = "Loss :" + result;
+
+
+            }
+
+        }
+    }
+}
