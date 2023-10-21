52085618a2872c6e39c0b200262d0269b8ac826d
Vrushali Mohite <vrushalimohite223@gmail.com>
2023-10-21 22:36:21 +0530

    windows

---
+++ Windows_form/form_if_else_electric_bill.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace form_if_else_electric_bill
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string name;
+            int id, unit;
+            float total=0;
+            float surchage = 0;
+            double netamount = 0;
+            name = (textBox1.Text);
+            id = Convert.ToInt32(textBox2.Text);
+            unit= Convert.ToInt32(textBox3.Text);
+            if(unit>0 && unit<=199)
+            {
+                total = unit * 1.20f;
+            }
+            else if(unit>=200 && unit<=400)
+            {
+                total = unit * 1.50f;
+            }
+            else if(unit>400 && unit<=600)
+            {
+                total = unit * 1.80f;
+            }
+            else if(unit>600)
+            {
+                total = unit * 2.00f;
+            }
+            if (total>=400)
+                surchage = total * 0.15f;
+            netamount = total + surchage;
+            label4.Text = "Employee Name :" + name;
+            label5.Text = "Employee Id :" + id;
+            label6.Text = "Unit consumed :" + unit;
+            label7.Text = "Amount Charge :" + total;
+            label8.Text = "Surchage Amount :" + surchage;
+            label9.Text = "Net Amount paid by the customed :" + netamount;
+
+
+
+        }
+    }
+}
---
+++ Windows_form/form_switch_dayno.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace form_no_dayno
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int dayno;
+            dayno = Convert.ToInt32(textBox1.Text);
+            switch (dayno)
+            {
+                case 1:
+                    label2.Text = "Monday";
+                    break;
+                case 2:
+                    label2.Text = "Tuesday";
+                    break;
+                case 3:
+                    label2.Text = "Wednesday";
+                    break;
+                case 4:
+                    label2.Text = "Thursdat";
+                    break;
+                case 5:
+                    label2.Text = "Friaday";
+                    break;
+                case 6:
+                    label2.Text ="Saturday";
+                    break;
+                case 7:
+                    label2.Text = "Sunday";
+                        break;
+                default:
+                    label2.Text = "Invalid Day No";
+                    break;
+            }
+           
+        }
+    }
+}
