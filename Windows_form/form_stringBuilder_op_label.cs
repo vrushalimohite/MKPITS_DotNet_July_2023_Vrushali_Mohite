f47c537f149afe47465a0bf67b02495daef36881
Vrushali Mohite <vrushalimohite223@gmail.com>
2023-10-25 20:48:15 +0530

    windows_form

---
+++ Windows_form/form_stringBuilder_op_label.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StringBuilder sb = new StringBuilder();
+            int res = 1;
+            int cnt;
+            int num = Convert.ToInt32(textBox1.Text);
+            for(cnt=1;cnt<=10;cnt++)
+            {
+                res = num * cnt;
+                sb.Append(num + "*" + cnt + "=" + res + "\n");
+
+            }
+            label2.Text = sb.ToString();
+
+        }
+    }
+}
---
+++ Windows_form/form_stringbuilder_op_richtextbox.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace form_string_builder_op_richtextbox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StringBuilder sb = new StringBuilder();
+            int res = 1;
+            int cnt;
+            int num = Convert.ToInt32(textBox1.Text);
+            for(cnt=1;cnt<=10;cnt++)
+            {
+                res = num * cnt;
+                sb.Append(num + "*" + cnt + "=" + res + "\n");
+            }
+            richTextBox1.Text = sb.ToString();
+        }
+    }
+}
