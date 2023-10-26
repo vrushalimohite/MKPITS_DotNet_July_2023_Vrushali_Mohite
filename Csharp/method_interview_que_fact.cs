6f1561d22ef5e88e7fb1cb1c86f60511b0791ffb
Vrushali Mohite <vrushalimohite223@gmail.com>
2023-10-26 22:56:59 +0530

    Csharp/

---
+++ Csharp/method_interview_que_fact.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace program
+{
+    class Mainclass
+    {
+        public static  int firstfactorial(int num)
+        {
+            int fact = 1;
+            while(num>0)
+            {
+                fact = fact * num;
+                num--;
+            }
+            return fact;
+        }
+        static void Main()
+        {
+            int number;
+            Console.WriteLine("Enter number");
+            number = Convert.ToInt32(Console.ReadLine());
+            int result = firstfactorial(number);
+            Console.WriteLine("output :"+result);
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
