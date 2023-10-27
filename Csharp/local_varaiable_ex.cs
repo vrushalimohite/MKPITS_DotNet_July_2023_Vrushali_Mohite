9038ade6e901a8262726bb8284ab88a853d3ab8e
Vrushali Mohite <vrushalimohite223@gmail.com>
2023-10-27 06:35:40 +0530

    csharp

---
+++ Csharp/local_varaiable_ex.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace program
+{
+    class program
+    {
+        static void local()
+        {
+           int  num = 10;
+            Console.WriteLine("num is inside the method" + num);
+
+            
+        }
+        static void Main()
+        {
+
+
+            local();
+            int num = 5; 
+            Console.WriteLine("num is outside the method" + num);
+        }
+
+
+
+    }
+}
