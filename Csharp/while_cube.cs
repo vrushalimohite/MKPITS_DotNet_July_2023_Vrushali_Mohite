b9c8eb9ea03b82c2ec2ed4afe5cb8d2b634b7e67
vrushalimohite <vrushalimohite223@gmail.com>
2023-10-28 17:24:41 +0530

    csharp

---
+++ Csharp/while_cube.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, cube = 0;
+            Console.WriteLine("Enter Number");
+            num = Convert.ToInt32(Console.ReadLine());
+            while(num>0)
+            {
+                cube = num * num;
+                Console.WriteLine("cube of given number is :"+" "+cube);
+                break;
+
+
+            }
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
