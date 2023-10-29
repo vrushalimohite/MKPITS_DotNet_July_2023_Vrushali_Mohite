385be05bc597d7b5e48d2781cbc8b98d6a717c9b
vrushalimohite <vrushalimohite223@gmail.com>
2023-10-29 23:48:14 +0530

    csharp

---
+++ Csharp/for_loop_print_table.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace for_loop_table
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = 0;
+            int num;
+            Console.WriteLine("Enter number");
+            num = Convert.ToInt32(Console.ReadLine());
+            for(int cnt = 1; cnt <= 10; cnt++)
+            {
+                result = num * cnt;
+                Console.WriteLine("{0}*{1}={2}",num,cnt,result);
+            }
+            Console.ReadKey();
+        }
+    }
+}
