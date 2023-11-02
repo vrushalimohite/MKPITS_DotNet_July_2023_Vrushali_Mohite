3b47d09a1d4d330311aba79f7e655e339671cdca
vrushalimohite <vrushalimohite223@gmail.com>
2023-11-02 19:41:43 +0530

    local

---
+++ Windows_form/class_Employee.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace class_employee_highest_sal_form
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Employee[] emp = new Employee[3];
+        int cnt = 0;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            emp[0] = new Employee();          
+            emp[1] = new Employee();
+
+            emp[2] = new Employee();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (cnt < 3)
+            {
+                emp[cnt].EmployeeID = Convert.ToInt32(textBox1.Text);
+                emp[cnt].Name = textBox2.Text;
+                emp[cnt].Salary = Convert.ToInt32(textBox3.Text);
+                cnt++;
+                textBox1.Clear();
+                textBox2.Clear();
+                textBox3.Clear();
+                textBox1.Focus();
+                if(cnt==3)
+                {
+                    button1.Enabled = false;
+                    textBox1.Enabled = false;
+                    textBox2.Enabled = false;
+                    textBox3.Enabled = false;
+                    button2.Enabled = true;
+                    MessageBox.Show("Details of 3 Employeed Accepted :");
+
+
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Employee details are :" + "\n");
+            for(int i=0;i<3;i++)
+            {
+                sb.Append("----------------------------------"+"\n");
+                sb.Append("EmployeeID :" + emp[i].EmployeeID + "\n");          
+                sb.Append("EmployeeName :" + emp[i].Name + "\n");
+
+                sb.Append("Employee Salary :" + emp[i].Salary + "\n");
+
+                sb.Append("----------------------------------");
+
+            }
+            label4.Text = sb.ToString();
+            double high = emp[0].Salary;
+            int flag = 0;
+            for(int i=0;i<3;i++)
+            {
+                for(int j=i+1;j<3;j++)
+                {
+                    if (high <emp[i].Salary)
+                    {
+                        high = emp[i].Salary;
+                        flag = i;
+                    }
+                }
+            }
+            StringBuilder sb1 = new StringBuilder();
+            sb1.Append("Employee with highest salary :"+"\n");
+            sb1.Append("EmployeeID :" + emp[flag].EmployeeID + "\n"); 
+            sb1.Append("EmployeeName :" + emp[flag].Name + "\n");
+            sb1.Append("Employee Salary :" + emp[flag].Salary + "\n");
+            label5.Text = sb1.ToString();
+
+        }
+    }
+}
---
+++ Windows_form/class_students.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace class_students
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Students[] stud = new Students[10];
+        int cnt = 0;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            stud[0] = new Students();
+            stud[1] = new Students();
+            stud[2] = new Students();
+            stud[3] = new Students();
+            stud[4] = new Students();
+            stud[5] = new Students();
+            stud[6] = new Students();
+            stud[7] = new Students();
+            stud[8] = new Students();
+            stud[9] = new Students();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(cnt<10)
+            {
+                stud[cnt].Name = textBox1.Text;
+                stud[cnt].Roll_no = Convert.ToInt32(textBox2.Text);
+                stud[cnt].Fees = Convert.ToDouble(textBox3.Text);
+                stud[cnt].Date = Convert.ToInt32(textBox4.Text);
+
+                cnt++;
+                textBox1.Clear();
+                textBox2.Clear();
+                textBox3.Clear();
+                textBox4.Clear();
+                textBox1.Focus();
+                if(cnt==10)
+                {
+                    button1.Enabled = false;
+                    textBox1.Enabled = false;
+                    textBox2.Enabled = false;
+                    textBox3.Enabled = false;
+                    textBox4.Enabled = false;
+                    button2.Enabled=true;
+                    MessageBox.Show("Highest Fees Paid students details ");
+
+
+                }
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Students Details are :");
+            for(int i=0; i<9;i++)
+            {
+                sb.Append("--------------------------"+"\n");
+                sb.Append("Students Name :" + stud[i].Name + "\n");
+                sb.Append("Students Roll No  :" + stud[i].Roll_no + "\n");
+                sb.Append("Students Fees :" + stud[i].Fees + "\n");
+                sb.Append("Fees Paid Date :" + stud[i].Date + "\n");
+                sb.Append("--------------------------"+"\n");
+
+            }
+            label4.Text = sb.ToString();
+            double high = stud[0].Fees;
+            int flag = 0;
+            for(int i=0;i<9;i++)
+            {
+                for(int j=0;j<9;j++)
+                {
+                    if (high < stud[i].Fees)
+                    {
+                        high = stud[i].Fees;
+                        flag = i;
+                    }
+                }
+            }
+            StringBuilder sb1 = new StringBuilder();
+            sb1.Append("Students Name :" + stud[flag].Name + "\n");
+            sb1.Append("Students Roll No  :" + stud[flag].Roll_no + "\n");
+            sb1.Append("Students Fees :" + stud[flag].Fees + "\n");
+            sb1.Append("Fees Paid Date :" + stud[flag].Date + "\n");
+            sb.Append("--------------------------");
+
+            label6.Text = sb1.ToString();
+        }
+    }
+}
+/*Class Code*/
+
+/*namespace class_students
+{
+    internal class Students
+    {
+        public string Name { get; set; }
+        public int Roll_no { get; set; }
+        public double Fees { get; set; }
+        public int Date { get; set; }
+    }
+}
+*/
