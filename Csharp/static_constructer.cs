830fd8898172571bfa2a702499c766c83c2d5018
vrushalimohite <vrushalimohite223@gmail.com>
2023-11-11 22:55:11 +0530

    csharp

---
+++ Csharp/static_constructer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    public class Account
+    {
+        public int id;
+        public String name;
+        public static float rateOfInterest;
+        public Account(int id, String name)
+        {
+            Console.WriteLine("instance constructor is called ");
+            this.id = id;
+            this.name = name;
+        }
+        static Account()
+        {
+            Console.WriteLine("static constructor is called ");
+            rateOfInterest = 9.5f;
+        }
+        public void display()
+        {
+
+            Console.WriteLine(id + " " + name + " " + rateOfInterest);
+        }
+    }
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+
+            Account a1 = new Account(101, "Sonoo");
+            Account a2 = new Account(102, "Mahesh");
+            a1.display();
+            a2.display();
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
---
+++ Csharp/static_object_counting.cs
@@ -0,0 +1,32 @@
+﻿using System;
+public class Account
+{
+    public int accno;
+    public String name;
+    public static int count = 0;
+    public Account(int accno, String name)
+    {
+        this.accno = accno;
+        this.name = name;
+        count++;
+    }
+
+    public void display()
+    {
+        Console.WriteLine(accno + " " + name);
+    }
+}
+class TestAccount
+{
+    public static void Main(string[] args)
+    {
+        Account a1 = new Account(101, "Sonoo");
+        Account a2 = new Account(102, "Mahesh");
+        Account a3 = new Account(103, "Ajeet");
+        a1.display();
+        a2.display();
+        a3.display();
+        Console.WriteLine("Total Objects are: " + Account.count);
+        Console.ReadKey();
+    }
+}
