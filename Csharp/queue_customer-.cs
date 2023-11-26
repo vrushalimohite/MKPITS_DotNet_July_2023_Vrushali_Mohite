b6c6a49cc20fe9b3aa1603fba0baa86308878927
vrushalimohite <vrushalimohite223@gmail.com>
2023-11-26 23:29:20 +0530

    local

---
+++ Csharp/queue_customer-.cs
@@ -0,0 +1,80 @@
+﻿
+
+/*create a queue collection of customer class(custid,custname,email,mobno)*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace customer
+{
+    class customer
+    {
+        public int custid;
+        public string custname;
+        public string email;
+        public double mono;
+        public customer(int custid, string custname, string email, double mono)
+        {
+            this.custid = custid;
+            this.custname = custname;
+            this.email = email;
+            this.mono = mono;
+        }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            customer c1 = new customer(123, "Rajesh", "Rajesh123@gmail.com", 5236418789);
+            customer c2 = new customer(2143, "Mahesh", "manu1254@gmail.com",1254878956);
+            customer c3 = new customer(55123, "Ridhi",  "ridhu254@gmail.Com",2156349875);
+            customer c4 = new customer(564123, "Shivay",  "shiv42@gmail.Com",4569812545);
+
+            Queue s = new Queue();
+            s.Enqueue(c1);
+            s.Enqueue(c2);
+            s.Enqueue(c3);
+            s.Enqueue(c4);
+            foreach (customer cus in s)
+            {
+                Console.WriteLine("Customer ID :"+cus.custid);
+                Console.WriteLine("Customer Name:" + cus.custname);
+                Console.WriteLine("Customer Gmail :" + cus.email);
+                Console.WriteLine("Customer Mobail NO:" + cus.mono);
+
+
+                Console.WriteLine("------------------------------------------");
+
+
+            }
+            customer p = (customer)s.Dequeue();
+            Console.WriteLine("------------------------------------------");
+            Console.WriteLine(" ");
+
+
+            Console.WriteLine("After Pop");
+            Console.WriteLine(" ");
+
+            foreach (customer cus in s)
+            {
+                Console.WriteLine("Customer ID :" + cus.custid);
+                Console.WriteLine("Customer Name:" + cus.custname);
+                Console.WriteLine("Customer Gmail :" + cus.email);
+                Console.WriteLine("Customer Mobail NO:" + cus.mono);
+                Console.WriteLine(" ");
+
+
+            }
+
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
---
+++ Csharp/stack_employee.cs
@@ -0,0 +1,80 @@
+﻿
+
+/*create a stack  collection of employee class(empno,empname , salary and designation)*/
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace stack_employee
+{
+    class employee
+    {
+        public int empno;
+        public string empname;
+        public int salary;
+        public string designation;
+        public employee(int empno, string empname,int salary,string designation)
+        {
+            this.empno = empno;
+            this.empname = empname;
+            this.salary = salary;
+            this.designation = designation;
+        }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            employee e1 = new employee(123, "Rajesh", 5000, "Maneger");
+            employee e2 = new employee(2143, "Mahesh", 7000, "Developer");
+            employee e3 = new employee(55123, "Ridhi", 88000, "Coder");
+            employee e4 = new employee(564123, "Shivay", 15000, "Designer");
+
+            Stack s = new Stack();
+            s.Push(e1);
+            s.Push(e2);
+            s.Push(e3);
+            s.Push(e4);
+            foreach(employee  emp in s)
+            {
+                Console.WriteLine("Employee Number:" + emp.empno);
+                Console.WriteLine("Employee Name :" + emp.empname);
+                Console.WriteLine("Employee Salary :" + emp.salary);
+                Console.WriteLine("Employee Designation :" + emp.designation);
+                Console.WriteLine("------------------------------------------");
+
+
+            }
+            employee p = (employee)s.Pop();
+            Console.WriteLine(p.empno+" "+p.empname+" "+p.salary+" "+p.designation);
+            Console.WriteLine("------------------------------------------");
+            Console.WriteLine(" ");
+
+
+            Console.WriteLine("After Pop");
+            Console.WriteLine(" ");
+
+            foreach (employee emp in s)
+            {
+                Console.WriteLine("Employee no :"+emp.empno);
+                Console.WriteLine("Employee Name :" + emp.empname);
+                Console.WriteLine("Employee Salary :" + emp.salary);
+                Console.WriteLine("Employee Designation :" + emp.designation);
+                Console.WriteLine(" ");
+
+
+            }
+
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
