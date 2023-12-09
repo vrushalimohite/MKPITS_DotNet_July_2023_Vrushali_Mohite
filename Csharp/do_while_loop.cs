536c5a3f26f4ecb390fefdc1cd7c9f53dc535a96
vrushalimohite <vrushalimohite223@gmail.com>
2023-12-09 22:49:12 +0530

    local

---
+++ Csharp/do_while_loop.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace do_while
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 11;
+            do
+            {
+                Console.WriteLine("num={0}" , num);
+                num++;
+            }
+            while (num <= 5);
+            Console.WriteLine("bye");
+            Console.ReadKey();
+        }
+    }
+}
