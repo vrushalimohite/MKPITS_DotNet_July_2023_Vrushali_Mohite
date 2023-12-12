dff580b47a80b55ba8311b8616f1e34c1b009d56
vrushalimohite <vrushalimohite223@gmail.com>
2023-12-12 15:53:54 +0530

    local

---
+++ SQLserver/not_null_stud.sql
@@ -0,0 +1,6 @@
+create table stud(rollno int , studname varchar(100) not null)
+insert into stud (rollno,studname) values(5,'rakhi')
+select * from stud
+insert into stud (rollno,studname) values(8,' ')
+select * from stud
+
