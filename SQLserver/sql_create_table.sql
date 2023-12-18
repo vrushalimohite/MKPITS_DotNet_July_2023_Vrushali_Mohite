f4390b699fe6088221ce16903c136dc64b39f615
vrushalimohite <vrushalimohite223@gmail.com>
2023-12-18 15:05:55 +0530

    local

---
+++ SQLserver/sql_create_table.sql
@@ -0,0 +1,6 @@
+create table users(age int ,name varchar(50),city varchar(50) )
+insert into users values(12,'aboli','nagpur')
+insert into users values(18,'jigu','pune')
+insert into users values(16,'kanchan','Amravti')
+insert into users values(14,'priti','nagpur')
+select name from users where age>=10
