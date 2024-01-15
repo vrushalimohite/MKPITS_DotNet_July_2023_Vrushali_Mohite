2ab63f40780611cd6cb1ac033d6151efae9714e0
vrushalimohite <vrushalimohite223@gmail.com>
2024-01-15 23:09:56 +0530

    local

---
+++ ADO.net/typing_master_login.sql
@@ -0,0 +1,3 @@
+create table tmlogin(fullname varchar(500),emailAddress varchar(500),qualification varchar(500),city varchar(500))
+
+select * from tmlogin
---
+++ SQLserver/college_mag_login_db.sql
@@ -0,0 +1,18 @@
+create table users(userid int,usertype varchar(500))
+insert into users VALUES(1,'PRINCIPLE')
+insert into users VALUES(2,'VICE-PRINCIPLE')
+insert into users VALUES(3,'HOD')
+insert into users VALUES(4,'STAFF')
+insert into users VALUES(5,'STUDENTS')
+insert into users VALUES(6,'EMPLOYEE')
+ SELECT * FROM USERS
+
+ DROP Table users
+
+ -------------------------------------------------------------------
+
+ CREATE TABLE login1(USERTYPE varchar(500),USERNAME VARCHAR(500),PASSWORD VARCHAR(500))
+
+ SELECT * FROM login1
+
+ drop table login1
